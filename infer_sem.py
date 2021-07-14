5d4326fc5a63e394c138046133bf4e8dc13529a5
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2021-07-14 09:01:25 +0800

    evaluation in SBC done

--- infer_sem.py
+++ infer_sem.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     config.freeze()
 
     device = 'cuda' if config.cuda else 'cpu'
-
+    device = 'cpu'
     # Parse input paths
     rgb_lst = glob.glob(args.inp)
     if len(rgb_lst) == 0:
